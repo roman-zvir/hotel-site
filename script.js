8657ff2ba205ba83fbdf2aa253307d35998185dc
Roman Zvir <roman.zvir.pp.2023@lpnu.ua>
2025-01-17 18:44:57 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+new Swiper('.swiper', {
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
